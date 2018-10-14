7a9ac0f172ca119bfc305dabcef5558366289a4d
fankunpeng <2361598538@qq.com>
2018-10-14 21:18:52 +0800

    comment

--- count.py
+++ count.py
@@ -1,5 +1,10 @@
 # 递归版
 def count(lst, target):
+    '''
+    如果 是空数组 返回0
+    如果 数组中第一个元素满足要求， 则返回 1 + 除第一个元素外数组剩余部分满足要求的元素个数
+    否则 返回除第一个元素外数组剩余部分满足要求的元素个数
+    '''
     if len(lst) == 0:
         return 0
     if lst[0] == target:
@@ -23,7 +28,7 @@ def inner(counter, lst, target):
     if len(lst) == 0:
         return counter
     if lst[0] == target:
-        return inner(counter+1, lst[1:], target)
+        return inner(counter + 1, lst[1:], target)
     return inner(counter, lst[1:], target)
 
 
@@ -45,16 +50,9 @@ def while_count(lst, target):
     return counter
 
 
-'''
-如果 数组中没有数字 返回0
-如果 数组中第一个数字大于0 返回 1 + 剩余大于100的个数
-否则 返回剩余大于100 的个数
-'''
-
 if __name__ == '__main__':
     array = [1, 101, 2, 2, 202]
     print(count(array, 2))
     print(tail_count(array, 2))
     print(for_count(array, 2))
     print(while_count(array, 2))
-
